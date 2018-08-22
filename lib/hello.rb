7164578b0bd8a42680f2bb80fdbdd890346f226f
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 18:35:02 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,7 @@
 # call your method here!
 
 def hello_t(array)
+  if block_given?
     i = 0
  
   while i < array.length
@@ -8,6 +9,8 @@ def hello_t(array)
     i = i + 1
   end
  
-  array
+    array
+else 
+  
 end
 

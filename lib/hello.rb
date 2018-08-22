f18f5c2495040a70b98aa987925ec3e3316823e6
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 18:29:05 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,8 @@
 # call your method here!
 
-def hello_t(["Tim", "Tom", "Jim"])
-  i = 0 
-  
-    while i < array.length
-      yield array[i]
-      i = i + 1
+def hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
   end
 end
-
-
 
